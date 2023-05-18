65b1045cfa26d6ac67f37636bf89f37ae78f7b50
DivineDemon <supame123@gmail.com>
2023-05-18 19:33:13 +0500

    Display Prompts! 💄✨🚀

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  const handleSearch = (e) => {};
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPrompts(data);
+    };
+
+    fetchPrompts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form action="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tags or users"
+          value={searchText}
+          onChange={handleSearch}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
