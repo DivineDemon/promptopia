ae70587f6e38eccdd74cb31fc9780e5b8071940e
DivineDemon <supame123@gmail.com>
2023-05-18 16:34:06 +0500

    Auth API Route! ✨🚀

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
+import User from "@/models/user";
 import { connectDB } from "@/utils/db";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
@@ -9,10 +10,31 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session({ session }) { },
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectDB();
+
+      // Check if User Exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
       return true;
     } catch (error) {
       console.error(error.message);
