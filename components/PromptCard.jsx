5fd05455839571f2e8947a117d89e4bd460d56be
DivineDemon <supame123@gmail.com>
2023-05-22 17:33:59 +0500

    Check Other Profiles! 🚀✨

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -17,10 +17,23 @@ const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(""), 3000);
   };
 
+  const handleProfileClick = () => {
+    if (prompt.creator._id === session?.user.id) {
+      return router.push("/profile");
+    }
+
+    router.push(
+      `/profile/${prompt.creator._id}?name=${prompt.creator.username}`
+    );
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
             src={prompt.creator.image}
             alt="user_image"
